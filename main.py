b68a0ef6b97fd174c6461aadcfa9406549cc939a
scrum-3 <scrum3@mde.tw>
2023-06-05 06:15:41 +0000

    修改網頁標題

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# for replit
 from cmsimde import flaskapp
+from gevent.pywsgi import WSGIServer
 
-flaskapp.app.run(host="0.0.0.0", debug=True)
+#flaskapp.app.run(host="0.0.0.0", debug=True)
+http_server = WSGIServer(('0.0.0.0', 8080), flaskapp.app)
+http_server.serve_forever()
